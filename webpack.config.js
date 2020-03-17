c12c93e92a751f927c77b361c4df91ff58c2f453
yuanhao <yuanhao@kingxunlian.com>
2020-03-17 12:38:28 +0800

    splitchunks

--- webpack.config.js
+++ webpack.config.js
@@ -3,58 +3,66 @@ const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-module.exports = {
-  entry: {
-    index: './src/index.js',
-    news: './src/news.js'
-  },
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  devtool: 'inline-source-map',
-  mode: 'development',
-  module: {
-    rules: [
-      {
-        test: /\.html$/i,
-        loader: 'html-loader',
-      },
-      {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: [
-          'file-loader'
-        ]
-      }
-    ]
-  },
-  plugins: [
-    new webpack.ProvidePlugin({
-      $: 'jquery',
-      jQuery: 'jquery'
-    }),
-    new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin({
-      filename: '[name].[hash].css'
-    }),
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      filename: 'index.html',
-      excludeChunks:['news']
-    }),
-    new HtmlWebpackPlugin({
-      template: './src/news.html',
-      filename: 'news.html',
-      excludeChunks:['index']
-    }),
-    new HtmlWebpackPlugin({
-      template: './src/whitebook.html',
-      filename: 'whitebook.html',
-    }),
+module.exports = function (env, argv) {
+  return {
+    entry: {
+      index: './src/index.js',
+      news: './src/news.js'
+    },
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist')
+    },
+    devtool: env.production ? 'source-maps' : 'eval',
+    mode: env.production ? 'production' : 'development',
+    module: {
+      rules: [
+        {
+          test: /\.html$/i,
+          loader: 'html-loader',
+        },
+        {
+          test: /\.css$/i,
+          use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        },
+        {
+          test: /\.(png|svg|jpg|gif)$/,
+          use: [
+            'file-loader'
+          ]
+        }
+      ]
+    },
+    plugins: [
+      new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery'
+      }),
+      new CleanWebpackPlugin(),
+      new MiniCssExtractPlugin({
+        filename: '[name].css',
+        chunkFilename: '[id].css',
+      }),
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        filename: 'index.html',
+        excludeChunks: ['news']
+      }),
+      new HtmlWebpackPlugin({
+        template: './src/news.html',
+        filename: 'news.html',
+        excludeChunks: ['index']
+      }),
+      new HtmlWebpackPlugin({
+        template: './src/whitebook.html',
+        filename: 'whitebook.html',
+      }),
 
-  ],
+    ],
+    optimization: {
+      splitChunks: {
+        chunks: 'all',
+      }
+    }
+  }
 };
